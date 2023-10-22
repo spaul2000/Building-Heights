3434d893f58f5fc0c20b4d818b09f7ff8340eaad
spaul2000 <spaul2@stanford.edu>
2023-10-22 17:24:06 +0000

    changed data preprocessing to add train val test split

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 import csv
 from tqdm import tqdm
-
+import random
 # TO START: run 'gcloud auth application-default login' and login with your personal email
 
 def read_and_preprocess_n_geotiffs_from_gcs(bucket_name, folder_path, n, bands_to_keep, mask_band):
@@ -13,13 +13,13 @@ def read_and_preprocess_n_geotiffs_from_gcs(bucket_name, folder_path, n, bands_t
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     
-    blobs = list(bucket.list_blobs(prefix=folder_path))[:n]
+    blobs = list(bucket.list_blobs(prefix=folder_path))
     
     processed_data = []
     mask_data = []
     profiles = []  # Store profiles (metadata) for each cropped segment
     
-    for blob in tqdm(list(bucket.list_blobs(prefix=folder_path))[:n], desc="Processing GeoTIFFs"):
+    for blob in tqdm(list(bucket.list_blobs(prefix=folder_path)), desc="Processing GeoTIFFs"):
         # Read the GeoTIFF from GCS into memory
         in_memory_file = io.BytesIO(blob.download_as_bytes())
         
@@ -82,15 +82,31 @@ def save_filepaths_to_csv(saved_files, mask_files, csv_path):
     with open(csv_path, "w", newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["image_filepaths", "mask_filepaths", "split"])
-        for data_path, mask_path in zip(saved_files, mask_files):
-            csv_writer.writerow([data_path, mask_path, 'train'])
+        
+        # Determine the total number of data points
+        total_data_points = len(saved_files)
+        
+        # Generate random assignments for each data point to one of the three splits
+        split_assignments = ["train"] * int(0.8 * total_data_points) \
+                          + ["val"] * int(0.1 * total_data_points) \
+                          + ["test"] * int(0.1 * total_data_points)
+                          
+        # Shuffle the split assignments to randomize the order
+        random.shuffle(split_assignments)
+        
+        # If the data size is not divisible by 10, append the remaining splits randomly
+        while len(split_assignments) < total_data_points:
+            split_assignments.append(random.choice(["train", "val", "test"]))
+        
+        for (data_path, mask_path, split) in zip(saved_files, mask_files, split_assignments):
+            csv_writer.writerow([data_path, mask_path, split])
 
 def main(bucket_name = 'cs325b-building-height',
     folder_path = 'data/sat_img/tx_sample_gt_2000/',
     bands_to_keep = [1, 2, 15, 16, 17],
     mask_band = [36],
     n = 2,  # Number of geotiffs to read and preprocess
-    data_output_folder = "/home/spaul/group/data/test"
+    data_output_folder = "/home/spaul/group/data/full_tx2000_rgb_vv_vh"
     ):
     # Call the reading and preprocessing function
     processed_data, mask_data, profiles = read_and_preprocess_n_geotiffs_from_gcs(bucket_name, folder_path, n, bands_to_keep, mask_band)
