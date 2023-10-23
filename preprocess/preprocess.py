2313523985e2e50f584ed35d1bf9df37c8b3a8c9
spaul2000 <spaul2@stanford.edu>
2023-10-23 22:23:52 +0000

    for jenna

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -8,50 +8,64 @@ from tqdm import tqdm
 import random
 # TO START: run 'gcloud auth application-default login' and login with your personal email
 
-def read_and_preprocess_n_geotiffs_from_gcs(bucket_name, folder_path, n, bands_to_keep, mask_band):
+def get_masks_data_profiles_for_blob(blob, bands_to_keep, mask_band):
+    # Read the GeoTIFF from GCS into memory
+    in_memory_file = io.BytesIO(blob.download_as_bytes())
+    processed_data = []
+    mask_data = []
+    profiles = []  # Store profiles (metadata) for each cropped segment
+    with rasterio.open(in_memory_file) as src:
+        
+        # Extract desired bands
+        selected_bands = src.read(bands_to_keep)
+        mask_band_data = src.read(mask_band)
+        
+        # Append data and profiles to their respective lists
+        for y in range(0, selected_bands.shape[1], 128):
+            for x in range(0, selected_bands.shape[2], 128):
+                cropped_data = selected_bands[:, y:y+128, x:x+128]
+                cropped_mask = mask_band_data[:, y:y+128, x:x+128]
+                
+                if cropped_data.shape[1] == 128 and cropped_data.shape[2] == 128:
+                    processed_data.append(cropped_data)
+                    mask_data.append(cropped_mask)
+                    cropped_profile = src.profile.copy()
+                    cropped_profile["height"] = 128
+                    cropped_profile["width"] = 128
+                    cropped_profile["count"] = len(bands_to_keep)
+                    cropped_profile["transform"] = rasterio.windows.transform(
+                        window=rasterio.windows.Window(x, y, 128, 128),
+                        transform=src.transform
+                    )
+                    profiles.append(cropped_profile)
+    in_memory_file.close()
+    return processed_data, mask_data, profiles
+
+
+def read_and_preprocess_n_geotiffs_from_gcs(bucket_name, folder_path, n, bands_to_keep, mask_band, data_output_folder):
     """Read and preprocess the first 'n' GeoTIFFs from a folder in GCS."""
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     
-    blobs = list(bucket.list_blobs(prefix=folder_path))
+    blobs = list(bucket.list_blobs(prefix=folder_path))[:150]
+    saved_files_all = []
+    mask_files_all = []
+    count = 0
     
-    processed_data = []
-    mask_data = []
-    profiles = []  # Store profiles (metadata) for each cropped segment
-    
-    for blob in tqdm(list(bucket.list_blobs(prefix=folder_path)), desc="Processing GeoTIFFs"):
-        # Read the GeoTIFF from GCS into memory
-        in_memory_file = io.BytesIO(blob.download_as_bytes())
-        
-        with rasterio.open(in_memory_file) as src:
-            print(f"Height of GeoTIFF: {src.height}, Width of GeoTIFF: {src.width}")
-            
-            # Extract desired bands
-            selected_bands = src.read(bands_to_keep)
-            mask_band_data = src.read(mask_band)
-            
-            # Append data and profiles to their respective lists
-            for y in range(0, selected_bands.shape[1], 128):
-                for x in range(0, selected_bands.shape[2], 128):
-                    cropped_data = selected_bands[:, y:y+128, x:x+128]
-                    cropped_mask = mask_band_data[:, y:y+128, x:x+128]
-                    
-                    if cropped_data.shape[1] == 128 and cropped_data.shape[2] == 128:
-                        processed_data.append(cropped_data)
-                        mask_data.append(cropped_mask)
-                        cropped_profile = src.profile.copy()
-                        cropped_profile["height"] = 128
-                        cropped_profile["width"] = 128
-                        cropped_profile["count"] = len(bands_to_keep)
-                        cropped_profile["transform"] = rasterio.windows.transform(
-                            window=rasterio.windows.Window(x, y, 128, 128),
-                            transform=src.transform
-                        )
-                        profiles.append(cropped_profile)
-    
-    return processed_data, mask_data, profiles
+    for blob in tqdm(blobs, desc="Processing GeoTIFFs"):
 
-def save_cropped_tiffs(data_list, mask_list, data_output_folder, profiles, prefix="cropped"):
+        processed_data, mask_data, profiles = get_masks_data_profiles_for_blob(blob, bands_to_keep, mask_band)
+        saved_files, mask_files = save_cropped_tiffs(processed_data, mask_data, data_output_folder, profiles, count)
+        print(saved_files)
+        saved_files_all.extend(saved_files)
+        mask_files_all.extend(mask_files)
+        count += 1
+
+    csv_file_path = os.path.join(data_output_folder, 'metadata.csv')
+    save_filepaths_to_csv(saved_files_all, mask_files_all, csv_file_path)
+        
+
+def save_cropped_tiffs(data_list, mask_list, data_output_folder, profiles, prefix):
     """Save each data array in the list as a GeoTIFF file using the provided profiles."""
     images_path = os.path.join(data_output_folder,'images')
     masks_path = os.path.join(data_output_folder, 'masks')
@@ -105,22 +119,11 @@ def main(bucket_name = 'cs325b-building-height',
     folder_path = 'data/sat_img/tx_sample_gt_2000/',
     bands_to_keep = [1, 2, 15, 16, 17],
     mask_band = [36],
-    n = 2,  # Number of geotiffs to read and preprocess
-    data_output_folder = "/home/spaul/group/data/full_tx2000_rgb_vv_vh"
+    n = 216,  # Number of geotiffs to read and preprocess
+    data_output_folder = "/home/spaul/group/data/test_150"
     ):
     # Call the reading and preprocessing function
-    processed_data, mask_data, profiles = read_and_preprocess_n_geotiffs_from_gcs(bucket_name, folder_path, n, bands_to_keep, mask_band)
-
-    # Save the processed data to individual GeoTIFF files in local folders
-    saved_files, mask_files = save_cropped_tiffs(processed_data, mask_data, data_output_folder, profiles)
-
-    # Save the file paths to a CSV
-    csv_file_path = os.path.join(data_output_folder, 'metadata.csv')
-    save_filepaths_to_csv(saved_files, mask_files, csv_file_path)
-
-    # print the paths of the saved files
-    for file_path, mask_path in zip(saved_files, mask_files):
-        print(file_path, mask_path)
+    read_and_preprocess_n_geotiffs_from_gcs(bucket_name, folder_path, n, bands_to_keep, mask_band, data_output_folder)
 
 if __name__ == "__main__":
     main()
